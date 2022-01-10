0de1dcf8a509f381b57c50691e4d872c971f6dc1
srburke <shayray601@gmail.com>
2022-01-10 15:27:27 -0600

    Update ShannonB_Lab1.js

--- ShannonB_Lab1.js
+++ ShannonB_Lab1.js
@@ -1,6 +1,6 @@
 
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script type="text/javascript"></div>
+    <div id="chart_div"></div>
     google.charts.load('current', {'packages':['gantt']});
     google.charts.setOnLoadCallback(drawChart);
 
